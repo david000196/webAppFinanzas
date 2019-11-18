eb15e8048439cf65f07c2582955c3549fe639118
Eddy Valverde Garro <eddy.valverde.garro@gmail.com>
2019-11-18 14:10:54 -0600

    Ingreso test

--- src/app/component/categoria-ingreso/categoria-ingreso.component.ts
+++ src/app/component/categoria-ingreso/categoria-ingreso.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegistroCategoriaComponent } from './registro-categoria/registro-categoria.component';
 
 @Component({
   selector: 'app-categoria-ingreso',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaIngresoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
-
+  
+  clickAddIngreso() {
+    const modal = this.modalService.open(RegistroCategoriaComponent);    
+    modal.result.then(
+      //ngthis.handleModalIngresoFormClose.bind(this),
+      //this.handleModalIngresoFormClose.bind(this)
+    );
+  }
 }
