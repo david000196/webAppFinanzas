419150b52b722d4a0633b96294c4861152fd15aa
Eddy Valverde Garro <eddy.valverde.garro@gmail.com>
2019-11-19 17:46:54 -0600

    Angular 7 Email/ Password & Google Login Using Firebase Demo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,34 +1,119 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../service/authentication.service';
+ 
 @Component({
-  selector: 'app-login',
+  selector: 'login-root',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
-  public email: string = '';
-  public password: string = '';
-  
+export class LoginComponent {
+  title = 'firebaseLogin';
+ 
+  selectedVal: string;
+  responseMessage: string = '';
+  responseMessageType: string = '';
+  emailInput: string;
+  passwordInput: string;
+  isForgotPassword: boolean;
+  userDetails: any;
+ 
+ 
   constructor(
-    private formBuilder: FormBuilder,
-    private router: Router
+    private authService: AuthenticationService
   ) {
-
+    this.selectedVal = 'login';
+    this.isForgotPassword = false;
+ 
   }
-
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
+ 
+  // Comman Method to Show Message and Hide after 2 seconds
+  showMessage(type, msg) {
+    this.responseMessageType = type;
+    this.responseMessage = msg;
+    setTimeout(() => {
+      this.responseMessage = "";
+    }, 2000);
   }
-
-
+ 
+  // Called on switching Login/ Register tabs
+  public onValChange(val: string) {
+    this.showMessage("", "");
+    this.selectedVal = val;
+  }
+ 
+  // Check localStorage is having User Data
+  isUserLoggedIn() {
+    this.userDetails = this.authService.isUserLoggedIn();
+  }
+ 
+  // SignOut Firebase Session and Clean LocalStorage
+  logoutUser() {
+    this.authService.logout()
+      .then(res => {
+        console.log(res);
+        this.userDetails = undefined;
+        localStorage.removeItem('user');
+      }, err => {
+        this.showMessage("danger", err.message);
+      });
+  }
+ 
+  // Login user with  provided Email/ Password
+  loginUser() {
+    this.responseMessage = "";
+    this.authService.login(this.emailInput, this.passwordInput)
+      .then(res => {
+        console.log(res);
+        this.showMessage("success", "Successfully Logged In!");
+        this.isUserLoggedIn();
+      }, err => {
+        this.showMessage("danger", err.message);
+      });
+  }
+ 
+  // Register user with  provided Email/ Password
+  registerUser() {
+    this.authService.register(this.emailInput, this.passwordInput)
+      .then(res => {
+ 
+        // Send Varification link in email
+        this.authService.sendEmailVerification().then(res => {
+          console.log(res);
+          this.isForgotPassword = false;
+          this.showMessage("success", "Registration Successful! Please Verify Your Email");
+        }, err => {
+          this.showMessage("danger", err.message);
+        });
+        this.isUserLoggedIn();
+ 
+ 
+      }, err => {
+        this.showMessage("danger", err.message);
+      });
+  }
+ 
+  // Send link on given email to reset password
+  forgotPassword() {
+    this.authService.sendPasswordResetEmail(this.emailInput)
+      .then(res => {
+        console.log(res);
+        this.isForgotPassword = false;
+        this.showMessage("success", "Please Check Your Email");
+      }, err => {
+        this.showMessage("danger", err.message);
+      });
+  }
+ 
+  // Open Popup to Login with Google Account
+  googleLogin() {
+    this.authService.loginWithGoogle()
+      .then(res => {
+        console.log(res);
+        this.showMessage("success", "Successfully Logged In with Google");
+        this.isUserLoggedIn();
+      }, err => {
+        this.showMessage("danger", err.message);
+      });
+  }
+ 
 }
