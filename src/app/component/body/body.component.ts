f145fc549b899054208f5861a7ccfbf42ca64d47
david <davidkaiser96@hotmail.com>
2019-11-20 06:06:09 -0600

    cambios boton logout

--- src/app/component/body/body.component.ts
+++ src/app/component/body/body.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../service/authentication.service';
 
 @Component({
   selector: 'app-body',
@@ -8,19 +7,10 @@ import { AuthenticationService } from '../../service/authentication.service';
 })
 export class BodyComponent implements OnInit {
 
-  constructor(private authService: AuthenticationService) {}
+  constructor() {}
 
   ngOnInit() {
   }
 
-  logoutUser() {
-    this.authService.logout()
-      .then(res => {
-        console.log(res);
-        //this.userDetails = undefined;
-        localStorage.removeItem('user');
-      }, err => {
-        //this.showMessage("danger", err.message);
-      });
-  }
+ 
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  logoutUser() {
+    this.authService.logout()
+      .then(res => {
+        //this.userDetails = undefined;
+        localStorage.removeItem('user');
+      }, err => {
+        //this.showMessage("danger", err.message);
+      });
   }
 
 }
